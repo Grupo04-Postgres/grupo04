4e19427f5cecdc9fb93501f138632ffd2b6fe002
Lucianoleal97 <75091364+Lucianoleal97@users.noreply.github.com>
2025-02-19 13:31:53 -0300

    Update 04_SP_JuegoDePrueba.sql

--- Entrega03/SQLQuerys/04_SP_JuegoDePrueba.sql
+++ Entrega03/SQLQuerys/04_SP_JuegoDePrueba.sql
@@ -90,7 +90,7 @@ EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProduc
 GO
 
 -- Prueba 2: nombre invalido (Cadena Vacia)
-EXEC dbProducto.InsertarProducto @nombre = ' ', @precio = 26, @idLineaProducto;
+EXEC dbProducto.InsertarProducto @nombre = ' ', @precio = 123, @idLineaProducto = 2;
 GO
    
 -- Prueba 3: precio invalido (Valor menor a cero)
@@ -102,7 +102,7 @@ EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 'abc', @idLineaPro
 GO
    
 -- Prueba 5: idLineaProducto invalido (Valor Vacio)
-EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProducto = ;
+EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProducto ='';
 GO
 
 -- Prueba 6: idLineaProducto invalido (Valor No numerico)
@@ -118,11 +118,21 @@ EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProduc
 GO
 
 -- Prueba 9: PrecioReferencia invalido (Valor vacio)
-EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProducto = 1, @precioReferencia = ;
+EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProducto = 1, @precioReferencia = '';
 GO
 
-   
- 
+-- Prueba 10: cantidadUnitaria invalida (Valor vacio)
+EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProducto = 1, @precioReferencia =-1, @cantidadUnitaria='';
+GO
+
+-- Prueba 11: Producto Valido 
+EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProducto = 1, @precioReferencia =-1, @cantidadUnitaria= 3;
+GO
+
+   -- Prueba 12: Nombre invalido (Producto ya cargado)
+EXEC dbProducto.InsertarProducto @nombre ='Banana', @precio = 26, @idLineaProducto = 1, @precioReferencia =-1, @cantidadUnitaria= 3;
+GO
+    
 ---------------------------------------------------------------------
 -- CLIENTE --
 
